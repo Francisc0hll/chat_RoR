2f42520f7141bd68b504330a825d883ab02eaeb1
Francisco Javier Hernandez LLewellyn <j.francisco.hll@gmail.com>
2019-09-10 17:56:36 -0300

    js

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, uniqueness: true, presence: true
+  has_many :room_messages
 
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
